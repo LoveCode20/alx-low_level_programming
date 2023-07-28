bea0169ede08548c30f513f653693e5229b65d0e
LoveCode20 <amysweat20@gmail.com>
2023-07-27 21:38:26 -0700

    new update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,7 @@ void first(void) __attribute__ ((constructor));
  */
 void first(void)
 {
-	printf("This is amazing! yet it feel so good to be true,\n");
-	printf("My dreams are becoming a reality!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
